3dcfeee51c865ddcdaddcf8200234961593aa56e
elisapichierriqa <EPichierri@qa.com>
2021-01-12 12:34:12 +0000

    Pom and Schema modified

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,2 +1,4 @@
 create database if not exists ims;
-create table if not exists ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
+create table if not exists ims.customers(customerID int primary key auto_increment, first_name varchar(40), surname varchar(40));
+create table if not exists ims.items(itemID int primary key auto_increment, item_name varchar(40));
+create table if not exists ims.orders(orderID int primary key auto_increment, customerID int, itemID int));
