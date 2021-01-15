d0edd5c8d47a441c1b2558428d16c19586ba5b56
elisapichierriqa <EPichierri@qa.com>
2021-01-15 15:07:18 +0000

    Schema edits, initial ERD created

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,5 +1,28 @@
-create database if not exists IMS;
-create table if not exists ims.customers(customerID INT primary key auto_increment, first_name VARCHAR(40), surname VARCHAR(40));
-create table if not exists ims.items(itemID INT primary key auto_increment, item_name VARCHAR(40));
-create table if not exists ims.orders(orderID INT primary key auto_increment, foreign key (customerID) references customers(customerID), foreign key (itemID) references items(itemID)));
-create table if not exists ims.orderline(orderlineID INT primary key auto_increment, orderID INT NOT NULL, itemID INT NOT NULL, customerID INT NOT NULL, quantity INT NOT NULL, foreign key (orderID) references orders(orderID), foreign key(itemID) references items(itemID));
+CREATE DATABASE IF NOT EXISTS ims;
+CREATE TABLE IF NOT EXISTS ims.customers (
+    customerID INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(40),
+    surname VARCHAR(40)
+);
+CREATE TABLE IF NOT EXISTS ims.items (
+    itemID INT PRIMARY KEY AUTO_INCREMENT,
+    item_name VARCHAR(40)
+);
+CREATE TABLE IF NOT EXISTS ims.orders (
+    orderID INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (customerID)
+        REFERENCES customers (customerID),
+    FOREIGN KEY (itemID)
+        REFERENCES items (itemID)
+);
+CREATE TABLE IF NOT EXISTS ims.orderline (
+    orderlineID INT PRIMARY KEY AUTO_INCREMENT,
+    orderID INT NOT NULL,
+    itemID INT NOT NULL,
+    customerID INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (orderID)
+        REFERENCES orders (orderID),
+    FOREIGN KEY (itemID)
+        REFERENCES items (itemID)
+);
